ced52e09edeba323c4a9da48b8b11fca11629c14
myslmzz <158145481+myslmzz@users.noreply.github.com>
2025-07-06 22:24:55 +0800

    Delete logger.php

--- logger.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-$data = json_decode(file_get_contents("php://input"), true);
-$log = "[" . date("Y-m-d H:i:s") . "] " . json_encode($data) . PHP_EOL;
-file_put_contents("log.txt", $log, FILE_APPEND);
-?>
