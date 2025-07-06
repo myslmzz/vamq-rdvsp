15d2143a0f3609989db8620611b8f195a36ae9d8
myslmzz <158145481+myslmzz@users.noreply.github.com>
2025-07-06 22:34:46 +0800

    Delete testbot.php

--- testbot.php
+++
@@ -1,7 +0,0 @@
-<?php
-file_get_contents("https://api.telegram.org/bot8072043320:AAFGmu6xSgfwyvvWNWNjguf61mMRaMZrGjw/sendMessage?" . http_build_query([
-  "chat_id" => "5818175793",
-  "text" => "Test mesej dari PHP logger",
-  "parse_mode" => "Markdown"
-]));
-?>
